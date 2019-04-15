f21e29bdcc3d4e33ff2ed3e7616483b0918d4aa6
Elijah Florence <efloren@siue.edu>
2019-04-15 20:23:59 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,8 +1,11 @@
 
 def levitation_quiz
-	while
+	loop do
 	  puts "What is the spell that enacts levitation?"
 	  anwser = gets.chomp
+	  if anwser == "Wingardium Leviosa"
+	    break
+	  
 	  
 end
 
